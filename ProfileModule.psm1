2624d3c12fe89924f63c09993d4c2b4e408ca19c
Terje Nagel <tna@schultz.dk>
2025-04-03 13:26:45 +0200

    add show-gitaliases command

--- ProfileModule.psm1
+++ ProfileModule.psm1
@@ -33,64 +33,51 @@ function Convert-FromBase64 {
   }
 }
 
-function Add-Path {
-  <#
-      .SYNOPSIS
-        Adds a Directory to the Current Path
-      .DESCRIPTION
-        Add a directory to the current path.  This is useful for 
-        temporary changes to the path or, when run from your 
-        profile, for adjusting the path within your powershell 
-        prompt.
-      .EXAMPLE
-        Add-Path -Directory "C:\Program Files\Notepad++"
-      .PARAMETER Directory
-        The name of the directory to add to the current path.
-    #>
-  
-  [CmdletBinding()]
-  param (
-    [Parameter(
-      Mandatory = $True,
-      ValueFromPipeline = $True,
-      ValueFromPipelineByPropertyName = $True,
-      HelpMessage = 'What directory would you like to add?')]
-    [Alias('dir')]
-    [string[]]$Directory
-  )
-  
-  PROCESS {
-    $Path = $env:PATH.Split(';')
-  
-    foreach ($dir in $Directory) {
-      if ($Path -contains $dir) {
-        Write-Verbose "$dir is already present in PATH"
-      }
-      else {
-        if (-not (Test-Path $dir)) {
-          Write-Verbose "$dir does not exist in the filesystem"
-        }
-        else {
-          $Path = $dir + $Path
-        }
-      }
-    }
-  
-    $env:PATH = [String]::Join(';', $Path)
-  }
-}
-
 function Start-TestShell ([string]$Module) {
   pwsh -NoExit -NoProfile -Command {
     param($Module)
-    Import-Module ProfileModule  -DisableNameChecking
     if ($Module) {
       Import-Module -Name $Module
-      Write-Host "Imported module: " $Module
+      Write-Host -ForegroundColor DarkYellow "Imported module: " $Module
     }
     function prompt { 
       Write-Host -NoNewline -ForegroundColor Green "$($pwd.Path.Substring($pwd.Path.LastIndexOf("\"))) TEST";
       return ">"
     }
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineOption -EditMode Windows
   } -args $Module
+}
+
+function Show-PortsInUse {
+  Get-NetTCPConnection | ForEach-Object {
+    $processId = $_.OwningProcess
+    $process = Get-Process -Id $processId
+    $_ | Select-Object LocalAddress, LocalPort, State, @{Name = "ProcessName"; Expression = { $process.ProcessName } }
+    | Where-Object { ($_.State -eq "Established") -or ($_.State -eq "Listen") }
+  } | Sort-Object LocalPort | Format-Table -AutoSize
+}
+
+function Reload-Module {
+  param (
+    [string] $ModuleName
+  )
+  Remove-Module -Name $ModuleName -Force
+  Import-Module -Name $ModuleName
+}
+
+
+function Show-GitAliases {
+  $aliases = git config -l | Select-String -Pattern "alias" 
+  $objects = @(); # Create an empty array to store the objects
+  ForEach ($alias in $aliases) {
+    $arr = $alias.ToString().Replace("alias.", "").Split('='); 
+    $obj = New-Object PSCustomObject -Property @{
+      Alias = $arr[0]
+      Command = ($arr[1..($arr.Length-1)] | Join-String)
+    }
+    $objects += $obj;
+  }
+  $objects  | Format-Table -Property 'Alias','Command'
 }
